e776fa66de783123ee34932a5d72e03c75ae4486
myrzagul070 <myrzagulergesbekova@gmail.com>
2023-04-18 18:19:00 +0600

    hw4

---
+++ app.js
@@ -0,0 +1,39 @@
+const arr = [
+    null,
+    123,
+    undefined,
+    null,
+    'qwerty',
+    34,
+    65,
+    undefined,
+    null,
+    null,
+    43,
+    'aerg',
+    'пкцйк',
+    null,
+    'uioo',
+];
+
+const obj = [],
+    numb = [],
+    str = [],
+    undef = [],
+    newArray = [];
+
+arr.forEach((i) => {
+    typeof i === 'object'
+        ? obj.push(i)
+        : typeof i === 'string'
+            ? str.push(i)
+            : typeof i === 'number'
+                ? numb.push(i)
+                : typeof i === 'undefined'
+                    ? undef.push(i)
+                    : '';
+});
+newArray.push(str, undef, numb, obj);
+
+const res = newArray.sort((a, b) => b.length - a.length);
+console.log(res);
